e91210e4c6cdce2eaa104b550c79713d1791c601
Woody <nz.wgkim@gmail.com>
2022-06-25 07:58:54 +1200

    React JS #6 이벤트 처리(Handling Events)
        function showName(){
            console.log('Mike');
        }
    
        <button onClick={showName}>Show Name</button>
        <button onClick={()=>{
                console.log(29);
             }
        }>Show age</button>
    
    <input type='text' onChange={(e)=>{
                    console.log(e.target.value);
                }}></input>

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -8,6 +8,12 @@ import World from './World';
 import styles from './Hello.module.css';
 
 export default function Hello(){
+    function showName(){
+        console.log('Mike');
+    }
+    function showAge(){
+        console.log(30);
+    }
      // 두개 이상의 tag를 표현하고자 할때는 <> </> 빈 tag 혹은 <div> </div> div tag로 감싸야 한다.
     return (
         <div>  
@@ -20,6 +26,14 @@ export default function Hello(){
             <World />
             <World />
             <div className={styles.box}> from Hello</div>
+            <button onClick={showName}>Show Name</button>
+            <button onClick={()=>{
+                    showAge();
+                }
+            }>Show age</button>
+            <input type='text' onChange={(e)=>{
+                console.log(e.target.value);
+            }}></input>
         </div>
     )
         
