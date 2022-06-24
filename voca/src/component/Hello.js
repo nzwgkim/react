05f46a393f5cc7c292083883e0ca1782e1a4f765
Woody <nz.wgkim@gmail.com>
2022-06-25 07:26:44 +1200

    module css 사용하기 naming rule: componentName.module.css Hello.css와 App.css에 동일한 name을 가진 component를 정의하면 충돌이 생긴다. import styles from './App.module.css'; <div className={styles.box}>From App </div>

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -4,14 +4,22 @@
 
 // export default Hello;
 
-import World from './World'
+import World from './World';
+import styles from './Hello.module.css';
+
 export default function Hello(){
      // 두개 이상의 tag를 표현하고자 할때는 <> </> 빈 tag 혹은 <div> </div> div tag로 감싸야 한다.
     return (
         <div>  
-            <h1>Hello</h1>
+            <h1 style={{
+                color:'#f05',
+                borderRight:"30px solid #000",
+                marginBottom:'50px',
+                opacity:1,
+            }}>Hello</h1>
             <World />
             <World />
+            <div className={styles.box}> from Hello</div>
         </div>
     )
         
