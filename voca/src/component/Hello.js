eb544390cdb3e1fcdb2c7cf60a17e12d9dc22d4f
Woody <nz.wgkim@gmail.com>
2022-06-25 18:03:25 +1200

    React JS #7 state, useState 	state는 component가 가지고 있는 속성(attribute)값이다. 	state가 변하면, react는 자동으로 화면을 update시켜준다. 	React16.8 부터는 모든 component를 함수형으로 만들수 있다.(Hooks)
        const [name,setName] = useState('Mike');  
        // useState는 배열을 반환한다.
        // [ state 변수명, state를 바꿔주는 함수]
        function changeName(){
            setName(name === 'Mike'?'Jane':'Mike');
        }
    
        <button onClick={changeName}>{name}</button>

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -6,8 +6,18 @@
 
 import World from './World';
 import styles from './Hello.module.css';
+import { useState } from 'react';
 
 export default function Hello(){
+    // let name = 'Mike';
+    const [name,setName] = useState('Mike');  
+    // useState는 배열을 반환한다. 
+    // [ state 변수명, state를 바꿔주는 함수]
+
+
+    function changeName(){
+        setName(name === 'Mike'?'Jane':'Mike');
+    }
     function showName(){
         console.log('Mike');
     }
@@ -17,13 +27,6 @@ export default function Hello(){
      // 두개 이상의 tag를 표현하고자 할때는 <> </> 빈 tag 혹은 <div> </div> div tag로 감싸야 한다.
     return (
         <div>  
-            <h1 style={{
-                color:'#f05',
-                borderRight:"30px solid #000",
-                marginBottom:'50px',
-                opacity:1,
-            }}>Hello</h1>
-            <World />
             <World />
             <div className={styles.box}> from Hello</div>
             <button onClick={showName}>Show Name</button>
@@ -34,6 +37,10 @@ export default function Hello(){
             <input type='text' onChange={(e)=>{
                 console.log(e.target.value);
             }}></input>
+
+            <div>
+                <button onClick={changeName}>{name}</button>
+            </div>
         </div>
     )
         
