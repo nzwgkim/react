e412e92c87aa9a1c0cf97b5018fac07b0aadc88c
Woody <nz.wgkim@gmail.com>
2022-07-02 22:07:08 +1200

    Clean up

--- voca/src/App.js
+++ voca/src/App.js
@@ -4,15 +4,25 @@ import DayList from './component/DayList';
 import Header from './component/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmptyPage from './component/EmptyPage';
+import { useEffect } from 'react';
 
-const heavyWork(){
+const heavyWork=()=>{
   console.log('엄청 무거운 작업');
   return ['홍길동','성춘향'];
-}
+};
+
 function App() {
   const [names, setNames] = useState(()=>{
     return heavyWork();
-  })
+  });
+  const [input, setInput] = useState('');
+  const handleUpload = () =>{
+    setNames((prevState)=>{
+      return [input, ...prevState];
+    };
+  }
+  useEffect(()=>{},[names]);  // dependancy array
+
   return (
     <div className='App'>
       <BrowserRouter>
