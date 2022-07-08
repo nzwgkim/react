9a0d593848c5609f3bc10cdc6efb8ef7c6e1f29b
Woody <nz.wgkim@gmail.com>
2022-07-09 09:03:29 +1200

    useMemo 제대로 사용하기

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,23 +1,44 @@
-import React, { useEffect, useRef } from 'react';
 import './App.css';
+import {useState,useMemo} from 'react'
+
+const hardCalculator=(number)=>{
+  // consuming long time
+  console.log('hard calculate number=',number);
+  return number+10000;
+}
+const easyCalculator=(number)=>{
+  // consuming short time
+  console.log('easy calculate number=',number);
+  return number+1;
+}
 
 function App() {
-  const inputRef = useRef();
-  useEffect(()=>{
-    inputRef.current.focus();
-    // console.log(inputRef); 
-  },[]);
+  const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
 
-  const login = ()=>{
-    // alert('Welcome ${inputRef.current.value}!');
-    console.log(inputRef.current);
-    inputRef.current.focus();
-  };
-
+  const hardSum = useMemo(()=>hardCalculator(hardNumber),[hardNumber]);
+  const easySum = useMemo(()=>easyCalculator(easyNumber),[easyNumber]);
+    
   return (
     <div>
-      <input ref={inputRef} type='text' placeholder='username' />
-      <button onClick={login}>Login</button>
+      <h2> Hard Calculation</h2> 
+      <input 
+        type='number' 
+        value={hardNumber}
+        onChange={(e)=>{
+          console.log(e.target.value);
+          setHardNumber(parseInt(e.target.value));
+        }} />
+      <p> Hard Sum = {hardSum} </p>
+
+      <h2> Easy Calculation</h2> 
+      <input 
+        type='number' 
+        value={easyNumber} 
+        onChange={(e)=>{
+          setEasyNumber(parseInt(e.target.value));
+        }} />      
+      <p> Easy Sum = {easySum} </p>
     </div>
   );
 }
